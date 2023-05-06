f58389b2cd0f8e854e063bbee284ecf35e1e9fce
shahadat84sh <shahadat84sh@gmail.com>
2023-05-06 17:43:29 +0600

    to calculation

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
-    const {cart} = props
+    const {cart} = props;
+    let total = 0;
+    let Shipping = 0;
+    let quantity = 0;
+
+    for (const product of cart){
+        quantity = quantity + product.quantity
+        total = total + product.price* product.quantity;
+        Shipping = Shipping + product.shipping
+    }
+    const tax = parseFloat((total * 0.1 ).toFixed(2));
+    const grandTotal = total + Shipping + tax
     return (
         <div className='cart'>
             <h4>Order for Summary</h4>
-            <p>Selected Items : {cart.length}</p>
+            <p>Selected Items : {quantity}</p>
+            <p>Total Price: ${total} </p>
+            <p>Shipping : ${Shipping} </p>
+            <p>Tax: ${tax} </p>
+            <h4>Grand Total : {grandTotal.toFixed(2)} </h4>
         </div>
     );
 };
